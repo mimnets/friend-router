8edb5c549e50a2c0f2189f9bdb820aa1957fd8e4
MiMNets <mdmonirulislammonir@gmail.com>
2020-09-01 08:39:34 +0600

    basic friend list setup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { useEffect } from 'react';
+import Friend from './components/Friend/Friend';
 
 function App() {
   const [friends, setFriends] = useState([])
@@ -14,6 +15,9 @@ function App() {
   return (
     <div>
       <h1>Friends {friends.length}</h1>
+      {
+        friends.map(friend => <Friend friend = {friend}></Friend>)
+      }
     </div>
   );
 }
